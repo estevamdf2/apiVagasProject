81f72cffc6dbd77f890c7060ad0e46e03b3c18a2
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-28 09:50:03 -0200

    Correção views aula10 - post inserção de registros

--- api/views.py
+++ api/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .serializers import *
+from rest_framework.response import Response
+from rest_framework import status
+from django.http import JsonResponse
 
-# Create your views here.
 class VagaList(APIView):
-    def get(self, request):
-           try:
-            lista_vagas = Vaga.objects.all()
-            paginator = PaginacaoVagas()
-            result_page = paginator.paginate_queryset(lista_vagas, request)
-            serializer = VagaSerializer(result_page, many=True)
-            return paginator.get_paginated_response(serializer.data)
-         Exception:
+    def post(self, request):
+        try:
+            serializer = VagaSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.erros, status=status.HTTP_400_BAD_REQUEST)
+        except Exception:
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
-                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
