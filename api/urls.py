4d5e0e67573d286d2be54894380204ec016d94bc
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-29 09:10:31 -0200

    Verbos get,put e delete e inclusão de nova url

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,5 @@ from .views import *
 
 urlpatterns = [
     url(r'^vagas$', VagaList.as_view()),
+    url(r'^vagas/(?P<pk>[0-9]+)$', VagaDetalhes.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -41,3 +41,42 @@ class VagaDetalhes(APIView):
             return JsonResponse({'Mensagem': "A vaga não existe."},status=status.HTTP_404_NOT_FOUND)
         except Exception:
             return JsonResponse({'Mensagem': "Ocorreu um erro no servidor."},status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    ''' Verbo para fazer a edição dos dados '''   
+    def put(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({'mensagem': "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            vaga = Vaga.objects.get(pk=pk)
+          
+            '''Criado um serializer nele e passado como parametro 
+            a vaga recuperada no banco e 
+            os dados preenchidos na requisição através do request.data'''
+            serializer = VagaSerializer(vaga, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
+            return Response(serializer.errors, 
+                status=status.HTTP_400_BAD_REQUEST)
+        except Vaga.DoesNotExist:
+            return JsonResponse({'mensagem': "A vaga não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def delete(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({'mensagem': "O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+                vaga = Vaga.objects.get(pk=pk)
+                vaga.delete()
+                return Response(status=status.HTTP_204_NO_CONTENT)
+        except Vaga.DoesNotExist:
+            return JsonResponse({'mensagem': "A vaga não existe"},
+                                status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+                                status=status.HTTP_500_INTERNAL_SERVER_ERROR)
