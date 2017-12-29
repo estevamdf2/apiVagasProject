518b59fb93eeca2b263097a3b0bc89594d3e6c19
Marcos Estevam de Sousa <marcos.sousa@ebc.com.br>
2017-12-29 08:17:11 -0200

    Exibindo registros por id

--- api/views.py
+++ api/views.py
@@ -16,3 +16,31 @@ class VagaList(APIView):
         except Exception:
             return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
                                 status=status.HTTP_500_INTERNAL_SERVER_ERROR) 
+    
+    ''' Responsável por exibir todos os registros salvos.'''
+    def get(self, request):
+        try:
+            lista_vagas = Vaga.objects.all()
+            serializer = VagaSerializer(lista_vagas, many=True)
+            return Response(serializer.data)
+        except Exception:
+            return JsonResponse({'mensagem': "Ocorreu um erro no servidor"},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+''' Classe responsável para usar verbos com parametros
+    get (findById) put e delete '''
+class VagaDetalhes(APIView):
+    def get(self, request, pk):
+        try:
+            if pk == "0":
+                return JsonResponse({"Mensagem: O ID deve ser maior que zero."},
+                                    status=status.HTTP_400_BAD_REQUEST)
+            vaga = Vaga.objects.get(pk=pk)
+            serializer = VagaSerializer(vaga)
+            return Response(serializer.data)
+        except Vaga.DoesNotExist:
+            return JsonResponse({"Mensagem: A vaga não existe."},
+                                    status=status.HTTP_404_NOT_FOUND)
+        except Exception:
+            return JsonResponse({"Mensagem: Ocorreu um erro no servidor."},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
